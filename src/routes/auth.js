f58202656c00616acd8d514d79f20dab24784d22
Carlos Carrillo <texcharly9@gmail.com>
2022-11-04 10:27:17 -0600

    Query by username login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,19 +5,23 @@ const { db } = require('../firebase')
 const auth = Router();
 
 auth.post('/login', async (req, res) => {
-    const {id} = req.body;
-    const object = await db.collection('users').doc(id).get();
-    console.log(object.data());
-    if(object.data()){
-        const user = {id : object.id , ...object.data()}
-        console.log(user)
+    const {username, password} = req.body;
+    console.log(username, " : ",password);
+    if(!username || !password){
+        res.status(400).send({error : 'Username o password no definidos'})
+        return;
+    }
+    const object = await db.collection('users').where('username', '==', username).get();
+    if(!object.docs.length){
+        const user = {id : object.docs[0].id , ...object.docs[0].data()};
 
-        
+
 
         res.status(200).send(user);
-
+        return;
     }
 
+    res.status(404).send({error : 'Usuario o contraseña inválidos'})
     
 })
 
