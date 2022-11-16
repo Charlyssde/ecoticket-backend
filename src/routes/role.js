1a4eb42894b769af0b56a33a530c6bedbefafd0a
Carlos Carrillo <texcharly9@gmail.com>
2022-11-16 12:33:57 -0600

    Crud servicios roles

--- src/routes/role.js
+++ src/routes/role.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const {db} = require('../firebase')
+const {errorFunc} = require("express-fileupload/lib/utilities");
 
 const role = Router();
 
@@ -18,8 +19,27 @@ role.get('/role/:id', async (req, res) => {
     })
 
     res.status(200).json(result)
-
 })
 
+role.put('/role/:id', async (req, res) => {
+    const id = req.params.id;
+    db.collection(collection).doc(id).update(req.body).then((result) => {
+        if(result.writeTime){
+            res.status(200).json({id : id});
+        }
+    }).catch((error) => {
+        console.log(error)
+        res.status(500).json({message : 'Ha ocurrido un error al actualizar'})
+    })
+})
+
+role.delete('/role/:id', async (req, res) => {
+    const id = req.params.id;
+    await db.collection(collection).doc(id).delete().then(() => {
+        res.status(200).json({message : 'success'})
+    }).catch((error) => {
+        res.status(500).json({message : error})
+    })
+})
 
 module.exports = role;
